7f49e181f6fe70d139fe0581da4592f77bea28eb
duypq3 <duypq3@cyberspace.vn>
2018-05-14 08:48:13 +0700

    add api

--- app/src/main/java/viettel/cyberspace/assitant/rest/ApiInterface.java
+++ app/src/main/java/viettel/cyberspace/assitant/rest/ApiInterface.java
@@ -6,15 +6,14 @@ import java.util.HashMap;
 import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
 import retrofit2.http.Query;
-import viettel.cyberspace.assitant.model.Respose;
+import viettel.cyberspace.assitant.model.Response;
 import viettel.cyberspace.assitant.model.StaticReponse;
 import viettel.cyberspace.assitant.model.User;
 
@@ -26,16 +25,21 @@ public interface ApiInterface {
 //    @POST("api/getContent")
 //    Call<AudioResponse> getContent(@Field("status") String status, @Header("Authorization") String token);
 
+    //new 2018
     @Headers("Content-Type: application/json")
-    @POST("api/statistic")
-    Call<StaticReponse> getStatistic(@Body HashMap<String, String> body, @Header("Authorization") String token);
-
-    @Multipart
-    @POST("api/saveAudio")
-    Call<Respose> saveAudio(@Part MultipartBody.Part file, @Query("id") String id, @Header("Authorization") String token);
+    @POST("va/login")
+    Call<User> login(@Body HashMap<String, String> body);
 
     @Headers("Content-Type: application/json")
-    @POST("api/register")
-    Call<Respose> register(@Body HashMap<String, String> body);
+    @POST("va/send-message")
+    Call<Response> sendMessage(@Body HashMap<String, String> body);
+
+    @Headers("Content-Type: application/json")
+    @GET("va/get-answer")
+    Call<Response> getAnswer(@Body HashMap<String, String> body);
+
+    @Headers("Content-Type: application/json")
+    @POST("va/rate-message")
+    Call<Response> rateMessage(@Body HashMap<String, String> body);
 
 }
